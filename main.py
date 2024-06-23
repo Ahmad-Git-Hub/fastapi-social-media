92998412c8317d217a63bb89f4a836e2b9b82d20
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-06-23 19:41:49 +0300

    Creat routes and implement functions for delete and update post handel HttpExceptions Return appropriate Http Status code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -8,14 +8,14 @@ app = FastAPI()
 
 # Schema of what should client send to creatposts endpoint
 class Post(BaseModel):
-    id: Optional[int]
+    id: Optional[int] = None
     title: str
     content: str
     published: bool = True
     rating: Optional[int] = None
 
 
-my_posts = [{"id": 592, "title": "Why people leave thier countries!", 
+my_posts = [{"id": 1, "title": "Why people leave thier countries!", 
             "content": "let's find out what happened during the 90's",
             "published": True, "rating": 4}]
 
@@ -37,23 +37,49 @@ def get_posts():
 
 @app.get("/posts/latest")
 def get_latest():
-    post = my_posts[len(my_posts)-1]
+    if len(my_posts) > 0:
+        post = my_posts[len(my_posts)-1]
+    else:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detail = f"No post was found!")
+
     return {'latest post:': post}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int):
     post = find_post(id)
-    if post:
-        return {"post": post}
-    else:
-        return "Post was not found!"
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail = f"Post with id {id} was not found!")
+    return {"post_details:" : post}
     
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     post_dict = post.model_dump()
     post_dict["id"] = randrange(0, 1000)
     my_posts.append(post_dict)
-    print(my_posts)
     return {"new post created": post_dict}
+
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+
+    post_to_delete = next((post for post in my_posts if post['id'] == id), None)
+
+    if post_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                           detail= f"post with id {id} does not exist!")
+
+    my_posts.remove(post_to_delete)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    post_to_update = next((post for post in my_posts if post['id'] == id), None)
+    if post_to_update is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                           detail= f"post with id {id} does not exist!")
+    else:
+        post_to_update.update(post.model_dump(exclude_unset=True))
+        return {"updated post": post_to_update}  
