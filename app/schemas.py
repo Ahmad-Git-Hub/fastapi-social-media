01c642b7d03562ac1866446e4a2e2108058c12f7
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-06-28 04:01:00 +0300

    Improve schemas class naming for better readability

--- app/schemas.py
+++ app/schemas.py
@@ -2,21 +2,18 @@ from datetime import datetime
 from pydantic import BaseModel, EmailStr
 
 # Schema of what should client send to path operation
-class PostBase(BaseModel):
+class PostBaseSchema(BaseModel):
     title: str
     content: str
     published: bool = True
 
 
-class PostCreate(PostBase):
-    pass
-
-class PostUpdate(PostBase):
+class PostInputSchema(PostBaseSchema):
     pass
 
 
 # Schema of what response should look like
-class PostResponse(PostBase):
+class PostResponseSchema(PostBaseSchema):
     id: int
     created_at: datetime
 
@@ -24,16 +21,18 @@ class PostResponse(PostBase):
         from_attributes = True
 
 
-class UserCreate(BaseModel):
+class UserResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class UserCreateSchema(BaseModel):
     email: EmailStr
     password: str
 
 
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    created_at: datetime
-
-    class Config:
-        from_attributes = True
 
