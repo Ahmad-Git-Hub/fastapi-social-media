5edc85ef7e76fc7dcc3b1f3e61052fdaa36e0feb
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-06-30 20:41:57 +0300

    Return user object instead of id in get_current_user method

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,15 +1,15 @@
 import jwt
 from fastapi import Depends, HTTPException, status
 from datetime import datetime, timedelta, timezone
-from app import models
-from . import variables, schemas
+from app import models, database, schemas, variables
 from jwt.exceptions import PyJWTError
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 
 ALGORITHM = variables.ALGORITHM
 SECRET = variables.SECRET
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -28,14 +28,23 @@ def verify_access_token(token: str, credentials_exception):
         id: str = payload.get('user_id')
         if id is None:
             raise credentials_exception
-        id = str(id)
-        token_data = schemas.TokenData(id=id)   
+        token_data = schemas.TokenData(id=str(id))   
     except PyJWTError:
         raise credentials_exception
     return token_data
     
 
-def get_current_user(token : str = Depends(oauth2_scheme)):    
+def get_current_user(token : str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):    
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
-    return verify_access_token(token, credentials_exception)
+    verified_token = verify_access_token(token, credentials_exception)
+
+    id = int(verified_token.id)
+    user = db.query(models.User).filter_by(id=id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail = f"User with id {id} was not found!"
+            )
+    return user
