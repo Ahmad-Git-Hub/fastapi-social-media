88388c5bc32afb1cfd0a233bb1475227391abccf
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-06-30 16:57:44 +0300

    Create scheme to validate token data returner by verify access token mehtod in oauth.py file

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
@@ -38,7 +39,12 @@ class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
-    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str]
 
 
 
