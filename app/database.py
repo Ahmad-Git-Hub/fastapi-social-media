333d1879a6bcdd297c4763aa962d6e0cfd772965
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-07-06 20:55:11 +0300

    Setup logging and handel error when connecting to database

--- app/database.py
+++ app/database.py
@@ -1,20 +1,47 @@
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from . import variables
+from app.config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{variables.user}:{variables.passowrd}@{variables.host}/{variables.dbname}"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+try:
+    DATABASE_URL = (
+        f"postgresql+psycopg://{settings.database_username}:"
+        f"{settings.database_password}@{settings.database_hostname}:"
+        f"{settings.database_port}/{settings.database_name}"
+    )
+
+    engine = create_engine(DATABASE_URL)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base = declarative_base()
+    logger.info("Database connection initialized successfully.")
+
+except Exception as e:
+    logger.error(f"Failed to initialize database connection: {str(e)}")
+    raise
+
+
+engine = create_engine(DATABASE_URL)
+
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 Base = declarative_base()
 
 
-# Dependency provide independent session per request 
 def get_db():
+    """
+    Dependency to provide an independent database session per request.
+
+    Yields:
+        SQLAlchemy Session: A database session.
+    """
     db = SessionLocal()
     try:
         yield db
