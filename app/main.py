0bc794771624049ea5c1b027d7ff5b9519c62b8a
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-07-06 16:57:33 +0300

    Remove unused imports from main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,12 @@
-from typing import List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-import psycopg
-import time
-from sqlalchemy.orm import Session
-from . import models, schemas, variables, utilities
-from .database import engine, get_db
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from app.routers import post, user, auth
 
 
-# Creates the tables
 models.Base.metadata.create_all(bind=engine)
-
-
 app = FastAPI()
-        
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
