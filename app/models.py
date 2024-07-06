ad19df4cd6638ca51c0d011213cfcc5ad9585e8b
Ahmad-Mohamad <ahmad.mohamad.cs@gmail.com>
2024-07-06 15:50:58 +0300

    Create sqlalchemy realtionship to return user information with post respone -Modify postResponse schema to return user info

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app.database import Base
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.orm import relationship
 
 
 class User(Base):
@@ -21,3 +22,4 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     user_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
+    post_owner = relationship("User")
--- app/schemas.py
+++ app/schemas.py
@@ -3,6 +3,19 @@ from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
+class UserResponseSchema(BaseModel):
+    user_id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class UserCreateSchema(BaseModel):
+    email: EmailStr
+    password: str
+
 class PostBaseSchema(BaseModel):
     title: str
     content: str
@@ -17,24 +30,10 @@ class PostResponseSchema(PostBaseSchema):
     user_id: int
     post_id: int
     created_at: datetime
+    post_owner: UserResponseSchema
 
     class Config:
         from_attributes = True
-
-
-class UserResponseSchema(BaseModel):
-    user_id: int
-    email: EmailStr
-    created_at: datetime
-
-    class Config:
-        from_attributes = True
-
-
-class UserCreateSchema(BaseModel):
-    email: EmailStr
-    password: str
-
 
 
 class UserLogin(BaseModel):
